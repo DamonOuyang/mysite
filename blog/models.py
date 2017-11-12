a1a0d0ea73d80afafc21e009fdf6cf14eda8f86d
DimonOuyang <825824032@qq.com>
2017-11-12 23:17:23 +0800

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class UserInfo(models.Model):
+    username = models.CharField(max_length=64)
+    sec = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+
+
+
+
+
+
+
+
+
+
---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,HttpResponse
+import datetime
+# Create your views here.
+
+def cur_time (request):
+
+    time = datetime.datetime.now()
+    # return  HttpResponse("<h1>ok</h1>")
+    return render(request, "cur_time.html", {"abc": time})
+
+
+
+user_list=[]
+def userInfo (req):
+
+# req.POST {"username": name, "sex": sex}
+    if req.method == "POST":
+        username = req.POST.get("username", None)
+        sex = req.POST.get("sex", None)
+        email = req.POST.get("email", None)
+        user = {"username": username, "sex": sex, "email": email}
+        user_list.append(user)
+
+    return render(req, "index.html", {"user_list": user_list})
+
+
+
+
+
+
+
+
+
+
