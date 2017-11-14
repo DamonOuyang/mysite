3686a9a509b1ff8d6c67d7178affab4a08c26868
DimonOuyang <825824032@qq.com>
2017-11-14 08:18:43 +0800

    test

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 import datetime
+from blog import models
 # Create your views here.
 
 def cur_time (request):
@@ -11,16 +12,21 @@ def cur_time (request):
 
 
 user_list=[]
-def userInfo (req):
+def UserInfo (req):
 
 # req.POST {"username": name, "sex": sex}
     if req.method == "POST":
-        username = req.POST.get("username", None)
-        sex = req.POST.get("sex", None)
-        email = req.POST.get("email", None)
-        user = {"username": username, "sex": sex, "email": email}
-        user_list.append(user)
-
+        u = req.POST.get("username", None)
+        s = req.POST.get("sex", None)
+        e = req.POST.get("email", None)
+        # user = {"username": username, "sex": sex, "email": email}
+        # user_list.append(user)
+        models.UserInfo.objects.create(
+            username=u,
+            sex=s,
+            email=e,
+        )
+    user_list = models.UserInfo.objects.all()
     return render(req, "index.html", {"user_list": user_list})
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ from blog import views
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^cur_time",views.cur_time),
-    url(r"userInfo",views.userInfo),
+    url(r"userInfo",views.UserInfo),
 ]
