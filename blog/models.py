15f00984c663f5155b6f8816f3a3bdc33ef40fda
DimonOuyang <825824032@qq.com>
2017-11-14 08:18:16 +0800

    test

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class UserInfo(models.Model):
     username = models.CharField(max_length=64)
-    sec = models.CharField(max_length=64)
+    sex = models.CharField(max_length=64)
     email = models.CharField(max_length=64)
 
 
